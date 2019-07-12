984cc0d7245d9bfa5c8623944019dd44561de6e2
Yuri Campos <yuricampos1@gmail.com>
2019-07-12 15:58:11 -0300

    Cliente, TipoCliente,Telefonesm e endereços

--- src/main/java/com/yuri/cursomc/CursomcApplication.java
+++ src/main/java/com/yuri/cursomc/CursomcApplication.java
@@ -9,10 +9,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.yuri.cursomc.domain.Categoria;
 import com.yuri.cursomc.domain.Cidade;
+import com.yuri.cursomc.domain.Cliente;
+import com.yuri.cursomc.domain.Endereco;
 import com.yuri.cursomc.domain.Estado;
 import com.yuri.cursomc.domain.Produto;
+import com.yuri.cursomc.domain.enums.TipoCliente;
 import com.yuri.cursomc.repositories.CategoriaRepository;
 import com.yuri.cursomc.repositories.CidadeRepository;
+import com.yuri.cursomc.repositories.ClienteRepository;
+import com.yuri.cursomc.repositories.EnderecoRepository;
 import com.yuri.cursomc.repositories.EstadoRepository;
 import com.yuri.cursomc.repositories.ProdutoRepository;
 
@@ -27,54 +32,67 @@ public class CursomcApplication implements CommandLineRunner {
 	private EstadoRepository estadoRepository;
 	@Autowired
 	private CidadeRepository cidadeRepository;
-	
+	@Autowired
+	private ClienteRepository clienteRepository;
+	@Autowired
+	private EnderecoRepository enderecoRepository;
+
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
-		//declaração categorias
-		Categoria cat1 = new Categoria (null, "Informática");
-		Categoria cat2 = new Categoria (null, "Escritório");
-		
-		//declaração produtos
-		 Produto p1 = new Produto (null, "Computador", 2000.0);
-		 Produto p2 = new Produto (null, "Impressora", 800.00);
-		 Produto p3 = new Produto (null, "Mouse", 80.00);
-		 
-		
-		 //Associação de categorias com produtos
-		 cat1.getProdutos().addAll(Arrays.asList(p1,p2,p3));
-		 cat2.getProdutos().addAll(Arrays.asList(p2));
-		 
-		 //Associação de produtos com categorias
-		 p1.getCategorias().addAll(Arrays.asList(cat1));
-		 p2.getCategorias().addAll(Arrays.asList(cat1,cat2));
-		 p3.getCategorias().addAll(Arrays.asList(cat1));
-		 	 
-		 //Salvar
-		 categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
-		 produtoRepository.saveAll(Arrays.asList(p1,p2,p3)); 
-	
-		 //Declaração Estados
-		 Estado est1 = new Estado (null, "Minas Gerais");
-		 Estado est2 = new Estado (null,"São Paulo");
-		 
-		 //Declaração Cidades
-		 Cidade c1 = new Cidade(null, "Uberlândia", est1);
-		 Cidade c2 = new Cidade(null, "São Paulo", est2);
-		 Cidade c3 = new Cidade(null, "São José dos Campo", est2);
-		
-		 //Associação de Estados > Cidades
-		 est1.getCidades().addAll(Arrays.asList(c1));
-		 est2.getCidades().addAll(Arrays.asList(c2,c3));
-		 
-		//Salvar
-		 estadoRepository.saveAll(Arrays.asList(est1,est2));
-		 cidadeRepository.saveAll(Arrays.asList(c1,c2,c3)); 
-		
-		 
+		/*-------------CATEGORIA PRODRUTOS----------------*/
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+
+		Produto p1 = new Produto(null, "Computador", 2000.0);
+		Produto p2 = new Produto(null, "Impressora", 800.00);
+		Produto p3 = new Produto(null, "Mouse", 80.00);
+
+		// Associação de categorias com produtos
+		cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
+		cat2.getProdutos().addAll(Arrays.asList(p2));
+
+		// Associação de produtos com categorias
+		p1.getCategorias().addAll(Arrays.asList(cat1));
+		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
+		p3.getCategorias().addAll(Arrays.asList(cat1));
+
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
+
+		/*------------- ESTADO/CIDADES ----------------*/
+		// Declaração Estados
+		Estado est1 = new Estado(null, "Minas Gerais");
+		Estado est2 = new Estado(null, "São Paulo");
+
+		// Declaração Cidades
+		Cidade c1 = new Cidade(null, "Uberlândia", est1);
+		Cidade c2 = new Cidade(null, "São Paulo", est2);
+		Cidade c3 = new Cidade(null, "São José dos Campo", est2);
+
+		// Associação de Estados > Cidades
+		est1.getCidades().addAll(Arrays.asList(c1));
+		est2.getCidades().addAll(Arrays.asList(c2, c3));
+
+		// Salvar estados e cidades
+		estadoRepository.saveAll(Arrays.asList(est1, est2));
+		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
+
+		/*-------------CLIENTE ENDEREÇO----------------*/
+		Cliente cli1 = new Cliente(null, "Yuri Campos", "yuri@gmail.com", "003118008", TipoCliente.PESSOAFISICA);
+
+		cli1.getTelefones().addAll(Arrays.asList("4002-8922", "4777-7000"));
+
+		Endereco e1 = new Endereco(null, "Rua da Favela", "6969", "ap 206", "capão redondo", "34088090", cli1, c2);
+		Endereco e2 = new Endereco(null, "Avenida Matos", "105", "Sala 800", "Centro", "65088060", cli1, c1);
+
+		cli1.getEnderecos().addAll(Arrays.asList(e1, e2));
+
+		clienteRepository.saveAll(Arrays.asList(cli1));
+		enderecoRepository.saveAll(Arrays.asList(e1, e2));
 	}
 
 }
