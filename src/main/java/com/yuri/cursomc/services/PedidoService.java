22891708f1cced9c90d1f0cc83240aad928e5d31
Yuri Campos <yuricampos1@gmail.com>
2019-07-16 11:21:40 -0300

    endpoint /pedidos/{id} disponibilizado

---
+++ src/main/java/com/yuri/cursomc/services/PedidoService.java
@@ -0,0 +1,23 @@
+package com.yuri.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.yuri.cursomc.domain.Pedido;
+import com.yuri.cursomc.repositories.PedidoRepository;
+import com.yuri.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class PedidoService {
+
+	@Autowired
+	private PedidoRepository repo;
+	
+	public Pedido buscar(Integer id){
+		Optional<Pedido> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id:" + id + "tipo" + Pedido.class.getName()));
+	}
+}
