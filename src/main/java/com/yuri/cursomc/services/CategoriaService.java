e3ee9f09d9c03ae6bb78b048bef057b9dc41d439
Yuri Campos <yuricampos1@gmail.com>
2019-07-18 16:50:55 -0300

    Validação sintática com Bean Validation

--- src/main/java/com/yuri/cursomc/services/CategoriaService.java
+++ src/main/java/com/yuri/cursomc/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.yuri.cursomc.domain.Categoria;
+import com.yuri.cursomc.dto.CategoriaDTO;
 import com.yuri.cursomc.repositories.CategoriaRepository;
 import com.yuri.cursomc.services.exceptions.DataIntegrityException;
 import com.yuri.cursomc.services.exceptions.ObjectNotFoundException;
@@ -55,5 +56,9 @@ public class CategoriaService {
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageRequest);
 		
+	}
+	
+	public Categoria fromDTO(CategoriaDTO objDto) {
+		return new Categoria(objDto.getId(), objDto.getNome());
 	}
 }
