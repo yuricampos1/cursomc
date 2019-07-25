48511a05e765127a0b18415e57bc42e01636fb22
Yuri Campos <yuricampos1@gmail.com>
2019-07-25 11:13:08 -0300

    Validação customizada: email não repetido na inserção de Cliente

--- src/main/java/com/yuri/cursomc/repositories/ClienteRepository.java
+++ src/main/java/com/yuri/cursomc/repositories/ClienteRepository.java
@@ -2,10 +2,13 @@ package com.yuri.cursomc.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.yuri.cursomc.domain.Cliente;
 
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
 
+	@Transactional(readOnly=true)
+	Cliente findByEmail(String email);
 }
--- src/main/java/com/yuri/cursomc/services/validation/ClienteInsertValidator.java
+++ src/main/java/com/yuri/cursomc/services/validation/ClienteInsertValidator.java
@@ -6,12 +6,19 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.yuri.cursomc.domain.Cliente;
 import com.yuri.cursomc.domain.enums.TipoCliente;
 import com.yuri.cursomc.dto.ClienteNewDTO;
+import com.yuri.cursomc.repositories.ClienteRepository;
 import com.yuri.cursomc.resources.exception.FieldMessage;
 import com.yuri.cursomc.services.validation.utils.BR;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+	@Autowired
+	private ClienteRepository repo;
+	
 	@Override
 	public void initialize(ClienteInsert ann) {
 	}
@@ -24,6 +31,10 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 		}
 		if (objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod()) && !BR.isValidCNPJ(objDto.getCpfOuCnpj())) {
 			list.add(new FieldMessage ("cpfOuCnpj", "CNPJ Inválido"));
+		}
+		Cliente aux = repo.findByEmail(objDto.getEmail());
+		if(aux != null) {
+			list.add(new FieldMessage("email", "Email já existente"));
 		}
 		for (FieldMessage e : list) {
 			context.disableDefaultConstraintViolation();
