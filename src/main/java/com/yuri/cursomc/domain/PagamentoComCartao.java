df5754055a24d20ee3dba00321346ab769b31f4b
Yuri Campos <yuricampos1@gmail.com>
2019-07-30 10:56:59 -0300

    Inserindo Pedido

--- src/main/java/com/yuri/cursomc/domain/PagamentoComCartao.java
+++ src/main/java/com/yuri/cursomc/domain/PagamentoComCartao.java
@@ -2,32 +2,29 @@ package com.yuri.cursomc.domain;
 
 import javax.persistence.Entity;
 
+import com.fasterxml.jackson.annotation.JsonTypeName;
 import com.yuri.cursomc.domain.enums.EstadoPagamento;
 
 @Entity
-public class PagamentoComCartao extends Pagamento  {
+@JsonTypeName("pagamentoComCartao")
+public class PagamentoComCartao extends Pagamento {
 	private static final long serialVersionUID = 1L;
 
 	private Integer numeroDeParcelas;
 	
 	public PagamentoComCartao() {
 	}
-
-	public PagamentoComCartao(Integer id, EstadoPagamento estado, Pedido pedido,Integer numeroDeParcelas) {
+	public PagamentoComCartao(Integer id, EstadoPagamento estado, Pedido pedido, Integer numeroDeParcelas) {
 		super(id, estado, pedido);
 		this.numeroDeParcelas = numeroDeParcelas;
 	}
-
 	public Integer getNumeroDeParcelas() {
 		return numeroDeParcelas;
 	}
-
 	public void setNumeroDeParcelas(Integer numeroDeParcelas) {
 		this.numeroDeParcelas = numeroDeParcelas;
 	}
 	
 	
-	
-	
-
+		
 }
